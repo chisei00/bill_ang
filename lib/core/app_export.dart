41fc1843e40afc9079e031ae0521980b981daa25
erictanthegreat <130438297+erictanthegreat@users.noreply.github.com>
2024-04-23 13:57:52 +0800

    Delete lib directory

--- lib/core/app_export.dart
+++
@@ -1,7 +0,0 @@
-export 'package:bill_ang/core/utils/image_constant.dart';
-export 'package:bill_ang/core/utils/size_utils.dart';
-export 'package:bill_ang/routes/app_routes.dart';
-export 'package:bill_ang/theme/app_decoration.dart';
-export 'package:bill_ang/theme/custom_text_style.dart';
-export 'package:bill_ang/theme/theme_helper.dart';
-export 'package:bill_ang/widgets/custom_image_view.dart';
